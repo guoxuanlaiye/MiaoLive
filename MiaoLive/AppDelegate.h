9601d6d953d17d0a989fde8fb2c1dc8c51beba88
yingcan <yingcan@yingcandeimac.local>
2017-01-05 15:40:55 +0800

    Initial Commit

---
+++ MiaoLive/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MiaoLive
+//
+//  Created by yingcan on 17/1/5.
+//  Copyright © 2017年 Guoxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MiaoLive/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MiaoLive
+//
+//  Created by yingcan on 17/1/5.
+//  Copyright © 2017年 Guoxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
